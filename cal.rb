6fc6cfff7995c40e40cda59bb4817e313835d475
tprax <tprax01@gmail.com>
2019-05-23 13:27:38 -0600

    asking info

---
+++ cal.rb
@@ -0,0 +1,21 @@
+#Calculator
+#Sudo Code
+#ask for first number
+#ask for (*/+-)
+#ask for second number
+#store answers
+#add
+#minus
+#multiple
+#divide
+
+puts "Welcome to a Calculator"
+puts "-----------------------"
+puts "Put the first number"
+@first_number = gets.to_i
+puts "Put in the operator Ex.(+ - * /)"
+@operator = gets.to_s.strip
+puts "Put in the second number"
+@second_number = gets.to_i
+
+puts "#{@first_number} #{@operator} #{@second_number}"
