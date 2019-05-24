91dfae66278589d9679a5b165cd4628738dec37a
tprax <tprax01@gmail.com>
2019-05-24 11:02:14 -0600

    floats

--- cal.rb
+++ cal.rb
@@ -8,28 +8,33 @@
 #minus
 #multiple
 #divide
+
 def calculator
-  puts "Welcome to a Calculator"
+  puts "Welcome to Ruby Calculator"
   puts "-----------------------"
   puts "Put the first number"
-  @first_number = gets.to_i
+  @first_number = gets.to_f
   puts "Put in the operator Ex.(+ - * /)"
   @operator = gets.to_s.strip
   puts "Put in the second number"
-  @second_number = gets.to_i
+  @second_number = gets.to_f
   case @operator
     when "+"
       answer = @first_number + @second_number
-      puts "#{@first_number} + #{@second_number} = #{answer}"
+      puts "Calculating..."
+      puts "#{@first_number} plus #{@second_number} equals #{answer}"
     when "-"
       answer = @first_number - @second_number
-      puts "#{@first_number} - #{@second_number} = #{answer}"
+      puts "Calculating..."
+      puts "#{@first_number} minus #{@second_number} equals #{answer}"
     when "*"
       answer = @first_number * @second_number
-      puts "#{@first_number} * #{@second_number} = #{answer}"
+      puts "Calculating..."
+      puts "#{@first_number} multiplied by #{@second_number} equals #{answer}"
     when "/"
       answer = @first_number / @second_number
-      puts "#{@first_number} / #{@second_number} = #{answer}"
+      puts "Calculating..."
+      puts "#{@first_number} divided by #{@second_number} equals #{answer}"
   end
 end
 
